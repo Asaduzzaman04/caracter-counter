213eb966b17f3122c5f83487dc8f0d59e70d5275
Asaduzzaman04 <asadchy152@gmail.com>
2024-07-27 01:58:15 +0600

    ramining carecter function added

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ inpText.addEventListener("keyup", () => {
 const display = () => {
   // ======> count-display-catch <===== \\
   const displayCount = document.getElementById("countDisplay");
-  displayCount.innerText = inpText.value.length;
+
+  displayCount.innerText =  inpText.value.length;
+
+//   =====> remaining-caracter <===== \\
+const displayRemain = document.getElementById("remainig-remaing");
+
+const maxlength = inpText.getAttribute('maxlength'); 
+
+displayRemain.innerText = maxlength - inpText.value.length;
 };
